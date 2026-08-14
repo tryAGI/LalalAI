d84a81de0febb55976bc244f2c5ceda20b909955
Konstantin S. <havendv@gmail.com>
2026-08-14 19:44:36 +0400

    feat: Updated OpenAPI spec (#66)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/LalalAI/Generated/LalalAI.LalalAIClient.g.cs
+++ src/libs/LalalAI/Generated/LalalAI.LalalAIClient.g.cs
@@ -44,10 +44,17 @@ namespace LalalAI
 
         /// <inheritdoc/>
         public global::LalalAI.AutoSDKClientOptions Options { get; }
+
+        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::LalalAI.SourceGenerationContext.Default);
+
         /// <summary>
         /// 
         /// </summary>
-        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext { get; set; } = global::LalalAI.SourceGenerationContext.Default;
+        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext
+        {
+            get => JsonSerializerContextProvider.Value;
+            set => JsonSerializerContextProvider = new(() => value);
+        }
 
 
         /// <summary>
@@ -56,7 +63,7 @@ namespace LalalAI
         public BatchStemSeparationClient BatchStemSeparation => new BatchStemSeparationClient(HttpClient, baseUri: null, authorizations: Authorizations, options: Options)
         {
             ReadResponseAsString = ReadResponseAsString,
-            JsonSerializerContext = JsonSerializerContext,
+            JsonSerializerContextProvider = JsonSerializerContextProvider,
         };
 
         /// <summary>
@@ -65,7 +72,7 @@ namespace LalalAI
         public CommonClient Common => new CommonClient(HttpClient, baseUri: null, authorizations: Authorizations, options: Options)
         {
             ReadResponseAsString = ReadResponseAsString,
-            JsonSerializerContext = JsonSerializerContext,
+            JsonSerializerContextProvider = JsonSerializerContextProvider,
         };
 
         /// <summary>
@@ -74,7 +81,7 @@ namespace LalalAI
         public StemSeparationClient StemSeparation => new StemSeparationClient(HttpClient, baseUri: null, authorizations: Authorizations, options: Options)
         {
             ReadResponseAsString = ReadResponseAsString,
-            JsonSerializerContext = JsonSerializerContext,
+            JsonSerializerContextProvider = JsonSerializerContextProvider,
         };
 
         /// <summary>
@@ -83,7 +90,7 @@ namespace LalalAI
         public VoiceChangeClient VoiceChange => new VoiceChangeClient(HttpClient, baseUri: null, authorizations: Authorizations, options: Options)
         {
             ReadResponseAsString = ReadResponseAsString,
-            JsonSerializerContext = JsonSerializerContext,
+            JsonSerializerContextProvider = JsonSerializerContextProvider,
         };
 
         /// <summary>
